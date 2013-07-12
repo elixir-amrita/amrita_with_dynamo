73d177f2fce1a388c8b31837afa12d6c58d8972f
josephwilk <joe@josephwilk.net>
2013-07-12 18:59:39 +0200

    annoying travis

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule AmritaDynamo.Mixfile do
 
   defp deps do
     [ { :cowboy, %r(.*), github: "extend/cowboy" },
-      { :dynamo, "0.1.0.dev", github: "elixir-lang/dynamo" },
+      { :dynamo, %r(.*) github: "elixir-lang/dynamo" },
       { :amrita, %r(.*), github: "josephwilk/amrita" } ]
   end
 end
